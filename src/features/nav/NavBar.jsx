283a2b954415992b57a68d55b67fbb90b7093e5c
Dhiraj Khapangi <dkhapangi13@outlook.com>
2021-09-21 13:43:08 +0545

    Adding menus for authenticated and unauthenticated users

--- src/features/nav/NavBar.jsx
+++ src/features/nav/NavBar.jsx
@@ -1,5 +1,7 @@
 import { NavLink } from 'react-router-dom'
 import { Button, Container, Menu } from 'semantic-ui-react'
+import SignedInMenu from './SignedInMenu'
+import SignedOutMenu from './SignedOutMenu'
 
 export default function NavBar({ setFormOpen }) {
   return (
@@ -18,15 +20,8 @@ export default function NavBar({ setFormOpen }) {
             content='Create Event'
           />
         </Menu.Item>
-        <Menu.Item position='right'>
-          <Button basic inverted content='Login' />
-          <Button
-            basic
-            inverted
-            content='Register'
-            style={{ marginLeft: '0.5em' }}
-          />
-        </Menu.Item>
+        <SignedOutMenu />
+        <SignedInMenu />
       </Container>
     </Menu>
   )
