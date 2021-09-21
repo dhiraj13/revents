ebaa2d1bdd4bbc507d66a57c6f1f4b513c903902
Dhiraj Khapangi <dkhapangi13@outlook.com>
2021-09-21 16:50:51 +0545

    Cleaning up the unused code

--- src/app/layout/App.jsx
+++ src/app/layout/App.jsx
@@ -1,4 +1,3 @@
-import { useState } from 'react'
 import { Container } from 'semantic-ui-react'
 import { Route } from 'react-router-dom'
 
@@ -9,19 +8,6 @@ import EventDetailedPage from '../../features/events/eventDetailed/EventDetailed
 import EventForm from '../../features/events/eventForm/EventForm'
 
 function App() {
-  const [formOpen, setFormOpen] = useState(false)
-  const [selectedEvent, setSelectedEvent] = useState(null)
-
-  function handleSelectEvent(event) {
-    setSelectedEvent(event)
-    setFormOpen(true)
-  }
-
-  function handleCreateFormOpen() {
-    setSelectedEvent(null)
-    setFormOpen(true)
-  }
-
   return (
     <>
       <Route exact path='/' component={HomePage} />
@@ -29,11 +15,14 @@ function App() {
         path='/(.+)'
         render={() => (
           <>
-            <NavBar setFormOpen={handleCreateFormOpen} />
+            <NavBar />
             <Container className='main'>
               <Route exact path='/events' component={EventDashboard} />
               <Route path='/events/:id' component={EventDetailedPage} />
-              <Route path='/createEvent' component={EventForm} />
+              <Route
+                path={['/createEvent', '/manage/:id']}
+                component={EventForm}
+              />
             </Container>
           </>
         )}
--- src/features/events/eventDashboard/EventDashboard.jsx
+++ src/features/events/eventDashboard/EventDashboard.jsx
@@ -1,27 +1,10 @@
 import { useState } from 'react'
 import { Grid } from 'semantic-ui-react'
-import EventForm from '../eventForm/EventForm'
 import EventList from './EventList'
 import { sampleData } from '../../../app/api/sampleData'
 
-export default function EventDashboard({
-  formOpen,
-  selectEvent,
-  setFormOpen,
-  selectedEvent,
-}) {
+export default function EventDashboard() {
   const [events, setEvents] = useState(sampleData)
-
-  function handleCreateEvent(event) {
-    setEvents([...events, event])
-  }
-
-  function handleUpdateEvent(updatedEvent) {
-    setEvents(
-      events.map((evt) => (evt.id === updatedEvent.id ? updatedEvent : evt))
-    )
-    selectEvent(null)
-  }
 
   function handleDeleteEvent(eventId) {
     setEvents(events.filter((evt) => evt.id !== eventId))
@@ -30,23 +13,10 @@ export default function EventDashboard({
   return (
     <Grid>
       <Grid.Column width='10'>
-        <EventList
-          events={events}
-          selectEvent={selectEvent}
-          deleteEvent={handleDeleteEvent}
-        />
+        <EventList events={events} deleteEvent={handleDeleteEvent} />
       </Grid.Column>
       <Grid.Column width='6'>
-        {formOpen && (
-          <EventForm
-            setEvents={setEvents}
-            setFormOpen={setFormOpen}
-            createEvent={handleCreateEvent}
-            selectedEvent={selectedEvent}
-            updateEvent={handleUpdateEvent}
-            key={selectedEvent ? selectedEvent.id : null}
-          />
-        )}
+        <h2>Event Filters</h2>
       </Grid.Column>
     </Grid>
   )
