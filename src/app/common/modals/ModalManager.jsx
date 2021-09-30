e4da939bff7d4cf1885e2ade10244770fb81bf08
Dhiraj Khapangi <dkhapangi13@outlook.com>
2021-09-30 21:44:31 +0545

    Creating the sign up form

--- src/app/common/modals/ModalManager.jsx
+++ src/app/common/modals/ModalManager.jsx
@@ -1,9 +1,11 @@
 import { useSelector } from 'react-redux'
 import TestModal from '../../../features/sandbox/TestModal'
+import LoginForm from '../../../features/auth/LoginForm'
 
 export default function ModalManager() {
   const modalLookup = {
     TestModal,
+    LoginForm,
   }
   const currentModal = useSelector((state) => state.modals)
   let renderedModal
