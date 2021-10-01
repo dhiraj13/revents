6c9503d3473e452a1d53884aa3c7b3aa9726eba1
Dhiraj Khapangi <dkhapangi13@outlook.com>
2021-10-01 08:42:55 +0545

    Adding an auth reducer

--- src/app/store/rootReducer.js
+++ src/app/store/rootReducer.js
@@ -1,10 +1,12 @@
 import { combineReducers } from 'redux'
 import eventReducer from '../../features/events/eventReducer'
 import modalReducer from '../common/modals/modalReducer'
+import authReducer from '../../features/auth/authReducer'
 
 const staticReducers = {
   event: eventReducer,
   modals: modalReducer,
+  auth: authReducer,
 }
 
 export const createReducer = (asyncReducers = {}) =>
